69a10cea3c12c0f3d7714753f1892848b488d5fc
Poojasrisunkara <165466759+Poojasrisunkara@users.noreply.github.com>
2025-06-23 20:00:41 +0530

    Create flag_VIP.sql

---
+++ 2_Programming Languages/PL-SQL Programming/1_Control Structures/Code/flag_VIP.sql
@@ -0,0 +1,14 @@
+ALTER TABLE Customers ADD IsVIP CHAR(1);
+
+BEGIN
+    FOR cust IN (SELECT CustomerID, Balance FROM Customers) LOOP
+        IF cust.Balance > 10000 THEN
+            UPDATE Customers
+            SET IsVIP = 'Y'
+            WHERE CustomerID = cust.CustomerID;
+
+            DBMS_OUTPUT.PUT_LINE('Customer ' || cust.CustomerID || ' marked as VIP.');
+        END IF;
+    END LOOP;
+END;
+/
